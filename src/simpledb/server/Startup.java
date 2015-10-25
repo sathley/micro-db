293917fe7bf9d78115197947f434f04a86da09c1
Sushant Athley <sathley90@gmail.com>
2015-10-25 15:18:14 -0400

    added driver to main codebase

--- src/simpledb/server/Startup.java
+++ src/simpledb/server/Startup.java
@@ -1,6 +1,8 @@
 package simpledb.server;
 
 import simpledb.remote.*;
+
+import java.rmi.RemoteException;
 import java.rmi.registry.*;
 
 public class Startup {
@@ -9,7 +11,18 @@ public class Startup {
       SimpleDB.init(args[0]);
       
       // create a registry specific for the server on the default port
-      Registry reg = LocateRegistry.createRegistry(1099);
+
+      Registry reg;
+      try {
+         reg = LocateRegistry.getRegistry(1099);
+         reg.list();
+      }
+      catch (RemoteException e)
+      {
+         reg = LocateRegistry.createRegistry(1099);
+      }
+
+
       
       // and post the server entry in it
       RemoteDriver d = new RemoteDriverImpl();
